daf9e9539e25ffae97925e95267e4da24d4fc381
Dom Slee <domslee97@gmail.com>
2019-10-20 00:41:29 +1100

    Add pyswarms stuff

--- python/hi.py
+++ python/hi.py
@@ -1,25 +1,40 @@
-from pyswarm import pso
-import time
-import datetime
+import numpy as np
+from shell import shell
+import os
+import argparse
+import pyswarms as ps
+from random import randint
 
-def banana(x):
-  time.sleep(1.5)
-  x1 = x[0]
-  x2 = x[1]
-  return x1**4 - 2*x2*x1**2 + x2**2 + x1**2 - 2*x1 + 5
+class MyRunner:
+  def __init__(self, binary: str):
+    self._binary = binary
+    if not os.access(binary, os.X_OK):
+      raise ValueError("Binary must be executable")
 
-def con(x):
-  x1 = x[0]
-  x2 = x[1]
-  return [-(x1 + 0.25)**2 + 0.75*x2]
+  def run(self):
+    options = {'c1': 0.5, 'c2': 0.3, 'w': 0.9}
+    optimizer = ps.single.GlobalBestPSO(n_particles=200, dimensions=17, options=options, velocity_clamp=(0,5))
+    cost, pos = optimizer.optimize(self.run_particle, iters=1000)
+    print(cost, pos)
+
+  def run_particle(self, particle_vs):
+    result = np.zeros(len(particle_vs))
+    for i, vs in enumerate(particle_vs):
+      result[i] = self.run_one(vs)
+      print("result[%d] = %0.8f\n" % (i, result[i]))
+    return result
+  
+  def run_one(self, vs):
+    args = [self._binary] + [str(v) for v in vs]
+    exec_str = ' '.join(args)
+    ex = shell(exec_str)
+    return float(ex.output()[0])
 
 
 
-start = datetime.datetime.now()
-lb = [-3, -1]
-ub = [2, 6]
-
-xopt, fopt = pso(banana, lb, ub, f_ieqcons=con)
-end = datetime.datetime.now()
-print(end-start)
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("binary", help="binary used for PSO")
+  args = parser.parse_args()
+  MyRunner(args.binary).run()
 
