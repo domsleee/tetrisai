438e535705d7f58ea5431d1b3cbd972b88e9fa75
Dom Slee <domslee97@gmail.com>
2019-11-03 20:34:26 +1100

    Add default seed

--- src/pso/PieceSetGetter.hpp
+++ src/pso/PieceSetGetter.hpp
@@ -12,11 +12,14 @@ static const int NUM_BLOCKS_PER_GAME = 50 + 575 + 1200;
 struct PieceSetGetter {
   std::mt19937 rd_;
   std::uniform_int_distribution<> dis_{0, NUM_BLOCKS_TYPES};
+  const int DEFAULT_SEED = 88;
+  PieceSetGetter() {
+    rd_.seed(DEFAULT_SEED);
+  }
   void setSeed(int seed) { rd_.seed(seed); }
 
   std::vector<std::vector<BlockType>> getPieceSets(int numSets) {
     std::vector<std::vector<BlockType>> res(numSets);
-    int numBlocks = allBlockTypes.size();
     for (auto &game: res) {
       for (int i = 0; i < NUM_BLOCKS_PER_GAME; i++) {
         int block = dis_(rd_);
