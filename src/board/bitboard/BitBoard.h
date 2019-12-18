3bb69f8ffa8b6e1b1b677438d057332336779cf8
Dom Slee <domslee97@gmail.com>
2019-12-18 18:24:01 +1100

    Delete unused

--- src/board/bitboard/BitBoard.h
+++ src/board/bitboard/BitBoard.h
@@ -82,7 +82,6 @@ class BitPieceInfo {
   int getId() const { return id_; }
   int getRepId() const { return BitBoardPre::getRepIdFromId(id_); }
   std::vector<BitPieceInfo> getClosedRotN() const {
-
     std::vector<BitPieceInfo> vs{*this};
     for (int id: BitBoardPre::getOpenRotN(id_)) {
       auto piece = b_->getPieceFromId(id);
@@ -91,34 +90,6 @@ class BitPieceInfo {
       }
     }
     return vs;
-
-    
-    std::unordered_set<BitPieceInfo> v{*this};
-    for (auto rotD: {RotateDirection::ROTATE_AC, RotateDirection::ROTATE_C}) {
-      if (canRotate(rotD)) {
-        auto nx = rotate(rotD);
-        v.insert(nx);
-        if (nx.canRotate(rotD)) {
-          v.insert(nx.rotate(rotD));
-        }
-      }
-    }
-    return {v.begin(), v.end()};
-
-
-
-
-    std::set<BitPieceInfo> s1{v.begin(), v.end()}, s2{vs.begin(), vs.end()};
-    if (s1 != s2) {
-      printf("self: %d\n", id_);
-      printf("s1(v): "); for (auto v: s1) printf("%d ", v.getId()); printf("\n");
-      printf("s2(vs): "); for (auto v: s2) printf("%d ", v.getId()); printf("\n");
-      printBoardWithPiece(*b_, *this);
-      printf("openN size: %lu\n", BitBoardPre::getOpenRotN(id_).size());
-    }
-    assert(s1 == s2);
-
-    return {v.begin(), v.end()};
   }
   void print() const;
 
