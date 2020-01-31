ef5b27dd7a1b55dc2f96a66ee9a24cb1d36736a2
Dom Slee <domslee97@gmail.com>
2020-02-01 02:41:36 +1100

    Optimistic

--- src/shared/Config.hpp
+++ src/shared/Config.hpp
@@ -6,12 +6,14 @@ struct Config {
   int numGames = UNDEF;
   int numLines = UNDEF;
   int maxDropRem = UNDEF; // todo.
+  int startingLines = UNDEF;
 
   void print() {
     printf("seed: %d\n", seed);
     printf("numGames: %d\n", numGames);
     printf("numLines: %d\n", numLines);
     printf("maxDropRem: %d\n", maxDropRem);
+    printf("startingLines: %d\n", startingLines);
   }
 };
 
@@ -20,4 +22,5 @@ void applyConfig(NewEvaluateWeightings<MyRunPieceSet> &ew, const Config &config)
   if (config.seed != Config::UNDEF) ew.setSeed(config.seed);
   if (config.numGames != Config::UNDEF) ew.setNumGames(config.numGames);
   if (config.numLines != Config::UNDEF) ew.setNumLines(config.numLines);
+  if (config.startingLines != Config::UNDEF) ew.setStartingLines(config.startingLines);
 }
