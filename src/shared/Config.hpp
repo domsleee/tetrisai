e05633a7ec2b065df80db55996360a2a8f826502
Dom Slee <domslee97@gmail.com>
2020-02-01 12:46:18 +1100

    OK

--- src/shared/Config.hpp
+++ src/shared/Config.hpp
@@ -2,11 +2,6 @@
 #include <iostream>
 #include "src/pso/NewEvaluateWeightings.tpp"
 
-struct Config;
-template <typename MyRunPieceSet>
-void applyConfig(NewEvaluateWeightings<MyRunPieceSet> &ew, const Config &config);
-
-
 struct Config {
   static const int UNDEF = -1;
   int seed = UNDEF;
@@ -34,11 +29,3 @@ struct Config {
     if (startingLines != Config::UNDEF) ew.setStartingLines(startingLines);
   }
 };
-
-template <typename MyRunPieceSet>
-void applyConfig(NewEvaluateWeightings<MyRunPieceSet> &ew, const Config &config) {
-  if (config.seed != Config::UNDEF) ew.setSeed(config.seed);
-  if (config.numGames != Config::UNDEF) ew.setNumGames(config.numGames);
-  if (config.numLines != Config::UNDEF) ew.setNumLines(config.numLines);
-  if (config.startingLines != Config::UNDEF) ew.setStartingLines(config.startingLines);
-}
