02913933cc9013752251b4c412922498328eaffc
Dom Slee <domslee97@gmail.com>
2020-02-28 02:11:56 +1100

    missing stuff

--- src/pso/main/main_bench_time.cpp
+++ src/pso/main/main_bench_time.cpp
@@ -1,12 +1,16 @@
-
 #include "src/pso/ClientApi.hpp"
-#include "src/pso/main/get_moves_utils.hpp
+#include "src/pso/main/get_moves_utils.hpp"
+#include <chrono>
+
 
 int main(int argc, char ** argv) {
+  auto startTime = std::chrono::system_clock::now();
   auto me = getBestMoveEvaluatorBlockLinear(true);
   Config cfg;
   cfg.numGames = 100;
   cfg.seed = 55;
   cfg.print();
   get_score_regular(me, cfg);
+  auto endTime = std::chrono::system_clock::now();
+  printf("evaluate time: %ldms\n", getMs(endTime-endTimePrecompute));
 }
