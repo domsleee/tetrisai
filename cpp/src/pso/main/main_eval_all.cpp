a622cc65f1332df423a5b71711ca69c1568d6c90
Dom Slee <domslee1@gmail.com>
2020-03-08 17:22:44 +1100

    notes

---
+++ cpp/src/pso/main/main_eval_all.cpp
@@ -0,0 +1,17 @@
+
+int main() {
+  // file --> (name, config, group, weights)
+  // (config, group, weights) --> EvaluateResult
+
+  // + MoveFinder w/ lookahead (effort). Need to 
+  // + pairs w/ transition (easy). Specify array of pairs of names
+}
+
+/**
+ * Simple start
+ * getTransitionEvaluationWithLookahead(me1, me2)
+ * 
+ * GetEvaluateResult(file)
+ * 
+ * 
+ */
