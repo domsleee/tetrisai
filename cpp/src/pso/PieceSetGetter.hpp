5cb4237661b2db3c3d46adb78ce28e726a2bbdbe
Dom Slee <domslee1@gmail.com>
2020-04-02 22:55:17 +1100

    Update rng generator

--- cpp/src/pso/PieceSetGetter.hpp
+++ cpp/src/pso/PieceSetGetter.hpp
@@ -18,17 +18,22 @@ struct PieceSetGetter: public IPieceSetGetter {
   void setSeed(int seed) override { seed_ = seed; }
 
   std::vector<std::vector<BlockType>> getPieceSets(int numGames=NUM_GAMES) const override {
-    boost::mt19937 gen_;
-    boost::uniform_int<> dist_{0, NUM_BLOCK_TYPES-1};
-    boost::random::variate_generator<boost::mt19937&, boost::uniform_int<>> rd_{gen_, dist_};
-    gen_.seed(seed_);
+    boost::mt19937 gen, gen2;
+    boost::uniform_int<> dist{0, NUM_BLOCK_TYPES};
+    boost::uniform_int<> dist2{0, NUM_BLOCK_TYPES-1};
+
+    boost::random::variate_generator<boost::mt19937&, boost::uniform_int<>> rd1{gen, dist};
+    boost::random::variate_generator<boost::mt19937&, boost::uniform_int<>> rd2{gen2, dist2};
+
+    gen.seed(seed_);
+    gen2.seed(seed_+1);
 
     std::vector<std::vector<BlockType>> res(numGames);
     int last = -1;
     for (auto &game: res) {
       for (int i = 0; i < NUM_BLOCKS_PER_GAME; i++) {
-        int block = rd_();
-        if (block == last) block = rd_();
+        int block = rd1();
+        if (block == last || block == NUM_BLOCK_TYPES) block = rd2();
         game.push_back(allBlockTypes[block]);
         last = block;
       }
