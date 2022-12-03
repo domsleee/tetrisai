468643e0779c140f4cbd3548e7b6f0ac023e38e1
Dom Slee <domslee1@gmail.com>
2022-12-03 12:49:04 +1100

    maybe fix circleci?

--- python/common/common.py
+++ python/common/common.py
@@ -4,8 +4,8 @@ import numpy
 DIR = os.path.dirname(os.path.realpath(__file__))
 PROJ_DIR = os.path.join(DIR, '..')
 BAZEL_BIN_DIR = os.path.join(DIR, '..', '..', 'cpp', 'bazel-bin')
-DEFAULT_BINARY = os.path.join(PROJ_DIR, 'external_bin', 'ew')
-EW_BENCH_BINARY = os.path.join(BAZEL_BIN_DIR, 'src', 'pso', 'main', 'ew_bench')
-MOVE_FINDER_CACHE_BINARY = os.path.join(BAZEL_BIN_DIR, 'src', 'ew_move_finder_cache')
+DEFAULT_BINARY = os.path.join(WORKSPACE_DIR, "bazel-bin", "src", "pso", "main", "get_moves")# os.path.join(PROJ_DIR, 'external_bin', 'ew')
+BENCH_BINARY = os.path.join(BAZEL_BIN_DIR, 'src', 'pso', 'main', 'get_moves_bench')
+MOVE_FINDER_CACHE_BINARY = os.path.join(BAZEL_BIN_DIR, 'src', 'move_finder_cache')
 
 Particle = numpy.ndarray
