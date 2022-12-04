20e4da70ee068151ef68033abbd1590d401fbf0b
Dom Slee <domslee1@gmail.com>
2022-12-04 23:49:28 +1100

    fix web

--- web/tetrisai-web/src/components/GameRunner/PieceCoords.ts
+++ web/tetrisai-web/src/components/GameRunner/PieceCoords.ts
@@ -10,37 +10,37 @@ export class PieceCoords {
     switch (blockType/*, startingCoord */) {
       case Piece.I_PIECE: {
         return [
-          {r: 0, c: 3}, {r: 0, c: 4}, {r: 0, c: 5}, {r: 0, c: 6},
+          {r: 2, c: 3}, {r: 2, c: 4}, {r: 2, c: 5}, {r: 2, c: 6},
         ];
       }
       case Piece.S_PIECE: {
         return [
-          {r: 0, c: 5}, {r: 0, c: 6}, {r: 1, c: 4}, {r: 1, c: 5},
+          {r: 2, c: 5}, {r: 2, c: 6}, {r: 3, c: 4}, {r: 3, c: 5},
         ];
       }
       case Piece.Z_PIECE: {
         return [
-          {r: 0, c: 4}, {r: 0, c: 5}, {r: 1, c: 5}, {r: 1, c: 6},
+          {r: 2, c: 4}, {r: 2, c: 5}, {r: 3, c: 5}, {r: 3, c: 6},
         ];
       }
       case Piece.O_PIECE: {
         return [
-          {r: 0, c: 4}, {r: 0, c: 5}, {r: 1, c: 4}, {r: 1, c: 5},
+          {r: 2, c: 4}, {r: 2, c: 5}, {r: 3, c: 4}, {r: 3, c: 5},
         ];
       }
       case Piece.T_PIECE: {
         return [
-          {r: 0, c: 4}, {r: 0, c: 5}, {r: 0, c: 6}, {r: 1, c: 5},
+          {r: 2, c: 4}, {r: 2, c: 5}, {r: 2, c: 6}, {r: 3, c: 5},
         ];
       }
       case Piece.J_PIECE: {
         return [
-          {r: 0, c: 4}, {r: 0, c: 5}, {r: 0, c: 6}, {r: 1, c: 6},
+          {r: 2, c: 4}, {r: 2, c: 5}, {r: 2, c: 6}, {r: 3, c: 6},
         ];
       }
       case Piece.L_PIECE: {
         return [
-          {r: 0, c: 4}, {r: 0, c: 5}, {r: 0, c: 6}, {r: 1, c: 4},
+          {r: 2, c: 4}, {r: 2, c: 5}, {r: 2, c: 6}, {r: 3, c: 4},
         ];
       }
     }
